0516515f05c5723389788591d8cebff4116d0c7e
kevinjyee <kevinyee@utexas.edu>
2017-09-23 13:52:24 -0500

    oh well goodenough

--- Lab3/Ngram.py
+++ Lab3/Ngram.py
@@ -85,4 +85,4 @@ def gengram_sentence(corpus, N=3, sentence_count=5, start_seq=None):
 
 if __name__ == "__main__":
     corpus = preprocess_corpus("concatTXT.txt")
-    print gengram_sentence(corpus)
+    print gengram_sentence(corpus, start_seq = "Our enumeration")
