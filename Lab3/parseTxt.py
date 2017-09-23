8dff142393e778f8d346a9cca76a7b1cf843f7de
kevinjyee <kevinyee@utexas.edu>
2017-09-23 12:39:45 -0500

    added ngram

--- Lab3/parseTxt.py
+++ Lab3/parseTxt.py
@@ -1,15 +1,12 @@
 import os
 def parseTxt():
     directory = "C:/Users/kevjy/Documents/Fall2017/EE379K/Labs/DSLab/Lab3/scrapedTXT/"
-    outfile = open('train.txt', "r+")
+    outfile = open('concatTXT.txt', "r+")
     for x in os.listdir(directory):
         fname = os.path.join(directory, x)
-        if os.path.isfile(fname):
-            with open(fname) as f:
-                wordList = f.read().split()
-                for word in wordList:
-                    if word.isalpha():
-                        outfile.write(word + " ")
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
 
     outfile.close()
 
